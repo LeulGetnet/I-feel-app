f76dfa4b9874c6a14473b5fd08baaa3088be8da3
LeulGetnet <leul.g.engida@gmail.com>
2024-03-25 14:40:03 -0400

    test

--- src/main/java/com/example/feelappbackend/Music/AudioModel.java
+++ src/main/java/com/example/feelappbackend/Music/AudioModel.java
@@ -1,9 +1,51 @@
 package com.example.feelappbackend.Music;
 
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class AudioModel {
-   private long id;
-   private MusicMOdel musicmodel;
-   private String fileName;
-   private String fileType;
-   private long size;
+
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    private MusicMOdel musicmodel;
+    private String fileName;
+    private String fileType;
+    private long size;
+
+    public String getFileName() {
+        return fileName;
+    }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+    public String getFileType() {
+        return fileType;
+    }
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setMusicmodel(MusicMOdel musicmodel) {
+        this.musicmodel = musicmodel;
+    }
+    public MusicMOdel getMusicmodel() {
+        return musicmodel;
+    }
+    public void setSize(long size) {
+        this.size = size;
+    }
+    public long getSize() {
+        return size;
+    }
+
+
 }
